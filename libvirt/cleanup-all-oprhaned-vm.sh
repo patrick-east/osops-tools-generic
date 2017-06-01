403f493c95c407e2836030d28631ac692317e924
Patrick East <patrick.east@purestorage.com>
2017-06-01 12:10:59 -0700

    Customize the cleanup orphaned libvirt instances script
    This now is tailored for pc2 and can be run remotely

---
+++ libvirt/cleanup-all-oprhaned-vm.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -xe
+
+NODES=($(ls ~/projects/cinder_jenkins_data/puppet/pc2/nodes | sed 's/\.pp//g'))
+
+VIRSH_IDS=""
+for SERVER in $(nova list --all-tenants | grep public_mgmt | awk '{print $2;}'); do
+    VIRSH_IDS+="$(nova show ${SERVER} | grep 'OS-EXT-SRV-ATTR:instance_name' | awk '{print $4}')\n"
+done
+
+
+for NODE in "${NODES[@]}"; do
+    echo "Cleaning orphaned VM's on ${NODE}"
+    scp -i ~/.ssh/pure_root ./* root@${NODE}:/root/
+    ssh -i ~/.ssh/pure_root root@${NODE} "VIRSH_IDS='${VIRSH_IDS}' /root/cleanup-orphaned-vms.sh"
+done
